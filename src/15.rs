b2737cb7c74ff6c94c9f8c8f2b162c631b9c585d
tegenkamphinsley48 <tegenkamphinsley48@gmail.com>
2025-03-20 05:56:57 +0000

    Add 15.rs

---
+++ src/15.rs
@@ -0,0 +1,9 @@
+// Generate a random Rust code for your project on GitHub
+fn main() {
+    let my_vec = vec![1, 2, 3];
+    let mut sum = 0;
+    for i in my_vec {
+        sum += i;
+    }
+    println!("Sum: {}", sum);
+}
